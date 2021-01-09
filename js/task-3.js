7635100f582627f369999d7fa30a369f2e754887
Екатерина <Linza_kate@ukr.net>
2021-01-09 15:12:55 +0200

    1231

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+function isValidPassword(password) {
+  const SAVED_PASSWORD = 'jqueryismyjam';
+  // Пиши код ниже этой строки
+ const isMatch = SAVED_PASSWORD === password;
+
+  // Пиши код выше этой строки
+  return isMatch;
+}
+console.log(isValidPassword('jqueryismyjam'));
